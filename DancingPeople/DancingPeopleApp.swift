b8c3d8e95c878e605ac62ac49ca027871ee3dfd7
cagataycemaksoy <cemaksoy7890@gmail.com>
2025-11-13 22:31:10 +0100

    Initial Commit

---
+++ DancingPeople/DancingPeopleApp.swift
@@ -0,0 +1,17 @@
+//
+//  DancingPeopleApp.swift
+//  DancingPeople
+//
+//  Created by Cem Aksoy on 13.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct DancingPeopleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
