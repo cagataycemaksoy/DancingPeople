bad921b728c75842cedcb0f95654f43abdde2530
cagataycemaksoy <cemaksoy7890@gmail.com>
2025-11-13 23:17:47 +0100

    UI is implemented

--- DancingPeople/ContentView.swift
+++ DancingPeople/ContentView.swift
@@ -8,14 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+  @State private var dancingPerson = Image("dancingPerson1")
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+          Color.indigo.opacity(0.45).ignoresSafeArea()
+          
+          VStack {
+            Spacer()
+            Text("Ready to dance?")
+              .font(.largeTitle)
+              .fontWeight(.medium)
+              .foregroundStyle(.white.opacity(0.9))
+            //TODO: onAppear, jumping the letters on y-axis independent from each other with each having delay, til the dancing person is tapped. After it is tapped, text is hidden with animation.
+            
+            Spacer()
+            
+              dancingPerson
+              .resizable()
+              .scaledToFit()
+            
+            Spacer()
+            Spacer()
+            
+            
+            Button {
+              //TODO: Pick a photo
+            } label: {
+              Label("Pick Photo", systemImage: "photo.fill.on.rectangle.fill")
+            }
+            .buttonStyle(.borderedProminent)
+            .tint(.appPurple)
+            .shadow(radius: 4)
+           
+            Rectangle().frame(height: 0)
+
+          }
         }
-        .padding()
+        
+        
     }
 }
 
