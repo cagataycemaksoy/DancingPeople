bb6c2c49481728b5dfb8297d2b8cec6101de738d
cagataycemaksoy <cemaksoy7890@gmail.com>
2025-11-13 23:44:46 +0100

    Audio and animation added

--- DancingPeople/ContentView.swift
+++ DancingPeople/ContentView.swift
@@ -6,9 +6,13 @@
 //
 
 import SwiftUI
+import AVFAudio
 
 struct ContentView: View {
   @State private var dancingPerson = Image("dancingPerson1")
+  @State private var isFullSize = true
+  @State private var audioPlayer: AVAudioPlayer!
+  
     var body: some View {
         ZStack {
           Color.indigo.opacity(0.45).ignoresSafeArea()
@@ -26,10 +30,17 @@ struct ContentView: View {
               dancingPerson
               .resizable()
               .scaledToFit()
+              .scaleEffect(isFullSize ? 1.0 : 0.8)
+              .onTapGesture {
+                playAudio()
+                isFullSize = false
+                withAnimation(.spring(response: 0.4, dampingFraction: 0.45)) {
+                  isFullSize = true
+                }
+              }
             
             Spacer()
             Spacer()
-            
             
             Button {
               //TODO: Pick a photo
@@ -44,9 +55,24 @@ struct ContentView: View {
 
           }
         }
-        
-        
     }
+  
+  func playAudio() {
+    if audioPlayer != nil && audioPlayer.isPlaying {
+      audioPlayer.stop()
+    }
+    
+    guard let audioData = NSDataAsset(name: "kick") else {
+      fatalError("Failed to find the file.")
+    }
+    
+    do {
+      audioPlayer = try AVAudioPlayer(data: audioData.data)
+      audioPlayer.play()
+    } catch {
+      fatalError("Failed to load the audio file contents.")
+    }
+  }
 }
 
 #Preview {
